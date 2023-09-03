1430cdc60bc0a9b82f8f063ae191b881057943da
Harshith Revanna <harshinr.xworkz@gmail.com>
2023-09-03 11:23:11 +0530

    insert table task1

---
+++ place.sql
@@ -0,0 +1,41 @@
+USE BUSSTAND;
+
+CREATE TABLE PLACE(ID int primary key,busno int,location varchar(14),start varchar(15),end varchar(15),engine_no varchar(30),model_name varchar(8),model_no varchar(40),price long,driver_name varchar(15));
+
+select * from place;
+
+insert into place values(4,254,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(5,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(6,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(41,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(11,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(21,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(1,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(1,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(31,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(51,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(451,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(14,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(17,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(81,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(91,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(21,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(1,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(1,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(1,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(1,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(1,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(8,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(98,221,'bengaluru','majastic','karivamma_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(54,221,'bengaluru','majastic','chandra_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(56,221,'bengaluru','majastic','kamkshipalya','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(58,221,'bengaluru','majastic','ckm','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(1,221,'bengaluru','majastic','banshakari','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(4,221,'bengaluru','majastic','nadini_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(9,221,'bengaluru','majastic','lalbag','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(8,221,'bengaluru','majastic','mahalakshmi_layout','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(9,221,'bengaluru','majastic','kankpura','1df452','kirlosk','14de74',500000000,'harisha_hr');
+insert into place values(8,221,'bengaluru','majastic','shivmoga','1df452','kirlosk','14de74',500000000,'harisha');
+
+
+
